834012935a98f46cc4e64f07862341ab31bc192a
İrem Kurt <ytu.iremkurt@gmail.com>
2023-01-15 21:16:25 +0300

    DB connection test

---
+++ DBTest.sql
@@ -0,0 +1,27 @@
+-- New script in websites1673686793216vhvostdyfjvmizum.
+-- Date: Jan 15, 2023
+-- Time: 7:26:53 PM
+-- Author: 
+
+-- ------------------------------------------------------
+-- Changelog
+-- Change Date-YYYYMMDD-, Person 
+
+-- ------------------------------------------------------
+-- Websites table review
+-- ------------------------------------------------------
+
+SELECT
+	--count(*)
+	*
+FROM
+	public.websites;
+	
+-- ------------------------------------------------------
+-- Owners table review
+-- ------------------------------------------------------
+
+SELECT
+	* 
+FROM
+	public.owners;
